688fb3b9f91e98f7cd908f0f27d0a5252cf8200b
Matthew Reda <m.reda94@gmail.com>
2022-01-28 17:34:29 -0500

    Changed login form to use formik

--- app/javascript/pages/Login/Login.jsx
+++ app/javascript/pages/Login/Login.jsx
@@ -1,12 +1,19 @@
 import React, {useState, useEffect} from 'react';
 import {Form, Button} from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import * as yup from "yup";
+import {Formik} from "formik";
 import axios from "axios";
+
+
+const login_schema = yup.object().shape({
+    userName: yup.string().required("User name required"),
+    password: yup.string().required("Password required")
+})
 
 export default Login = ({ loggedIn, setLoggedIn, setUser }) => {
     let navigate = useNavigate();
-    let [userName, setUserName] = useState('');
-    let [password, setPassword] = useState('');
+    
     useEffect(() => {if(loggedIn){
         navigate('/')
     }})
@@ -15,8 +22,8 @@ export default Login = ({ loggedIn, setLoggedIn, setUser }) => {
             '/login', 
             {
                 user: {
-                    username: userName, 
-                    password: password}
+                    username: event.userName, 
+                    password: event.password}
             },
             {withCredentials: true}
             ).then((response) => {
@@ -33,7 +40,6 @@ export default Login = ({ loggedIn, setLoggedIn, setUser }) => {
                     }
                 })
             .catch((error) => {console.log(error)})
-        event.preventDefault()
         
     }
     
@@ -41,17 +47,34 @@ export default Login = ({ loggedIn, setLoggedIn, setUser }) => {
     return(
     <>
     <h1 className="m-4">Login</h1>
-    <Form className="mx-4" onSubmit={handleSubmit}>
+    <Formik 
+        onSubmit={handleSubmit} 
+        initialValues={{userName: '', password:''}}
+        validationSchema={login_schema}
+    >
+    {({handleSubmit, handleChange, values, errors, touched}) => (
+    <Form noValidate className="mx-4" onSubmit={handleSubmit}>
         <Form.Group controlId="Username">
             <Form.Label>Username</Form.Label>
-            <Form.Control type="text" required value={userName} onChange={event=>setUserName(event.target.value)}/>
+            <Form.Control type="text" name="userName" value={values.userName} onChange={handleChange}
+            isValid={touched.userName && !errors.userName}
+            isInvalid={!!errors.userName}
+            placeholder="User name"/>
+            <Form.Control.Feedback type='invalid'>{errors.userName}</Form.Control.Feedback>
         </Form.Group>
         <Form.Group className="my-4">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" required value={password} onChange={event=>(setPassword(event.target.value))}/>
+            <Form.Control type="password" name="password" value={values.password} onChange={handleChange}
+            isValid={touched.password && !errors.password}
+            isInvalid={!!errors.password}
+            />
+            <Form.Control.Feedback type='invalid'>{errors.password}</Form.Control.Feedback>
         </Form.Group>
         <Button type="submit" variant='secondary'>Submit</Button>
     </Form>
+    )
+    }
+    </Formik>
     </>
     )
 }
